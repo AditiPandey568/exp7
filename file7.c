a5d8493e7b92e0c73e92d9aa083fc8c5f5ab0fda
repo4 <22BDO10031>
2024-04-07 15:16:11 +0530

    upper_bound=100 and lower_bound=50

--- file7.c
+++ file7.c
@@ -5,12 +5,11 @@
 int main() 
 { 
 	// Set the upper bound for random numbers 
-	int upper_bound = 1000; 
+	int upper_bound = 100; 
 	// Set the lower bound for random numbers 
-	int lower_bound = 100; 
-
+	int lower_bound = 50; 
 	// Loop through 10 times 
-	for (int i = 0; i < 10; i++) { 
+	for (int i = 0; i < 50; i++) { 
 		// Generate a random number within the specified 
 		// bounds 
 		int value = rand() % (upper_bound - lower_bound + 1) 
